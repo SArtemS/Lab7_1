0af32d77da40c24a06f963d77eb67b76b79be92f
Artem Scherbinin <spiderr3333@gmail.com>
2023-12-18 15:04:03 +0300

    2.1

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from int_class import IntegrateThread
-from math import sin, cos, tan
+from math import sin, cos, tan, atan, pi
 from timeit import timeit
 
 if __name__ == '__main__':
     sin100000 = IntegrateThread(sin, 3, 5, 100000)
     cos100000 = IntegrateThread(cos, 3, 5, 100000)
     tan100000 = IntegrateThread(tan, 3, 5, 100000)
+    atan100000 = IntegrateThread(atan, 0, pi / 2, 100000)
     
     print("Function (sin, a = 3, b = 5, iterations = 100000) = " + str(sin100000.integrate()))
     print("Time = " + str(round(timeit("sin100000 = IntegrateThread(math.sin, 3, 5, 100000)\nsin100000.integrate()", setup="from int_class import IntegrateThread\nimport math", number=10), 4)))
@@ -33,3 +34,12 @@ if __name__ == '__main__':
 
     print("Function (tan, a = 3, b = 5, iterations = 100000, 4 threads) = " + str(tan100000.integrateThreads(4)))
     print("Time = " + str(round(timeit("tan100000 = IntegrateThread(math.tan, 3, 5, 100000)\ntan100000.integrateThreads(4)", setup="from int_class import IntegrateThread\nimport math", number=10), 4)))
+   
+    print("\nFunction (atan, a = 0, b = pi/2, iterations = 100000) = " + str(atan100000.integrate()))
+    print("Time = " + str(round(timeit("atan100000 = IntegrateThread(math.atan, 0, math.pi, 100000)\natan100000.integrate()", setup="from int_class import IntegrateThread\nimport math", number=10), 4)))
+    
+    print("Function (tan, a = 0, b = pi/2, iterations = 100000, 2 threads) = " + str(atan100000.integrateThreads(2)))
+    print("Time = " + str(round(timeit("atan100000 = IntegrateThread(math.atan, 0, math.pi, 100000)\natan100000.integrateThreads(2)", setup="from int_class import IntegrateThread\nimport math", number=10), 4)))
+
+    print("Function (tan, a = 0, b = pi/2, iterations = 100000, 4 threads) = " + str(atan100000.integrateThreads(4)))
+    print("Time = " + str(round(timeit("atan100000 = IntegrateThread(math.atan, 0, math.pi, 100000)\natan100000.integrateThreads(4)", setup="from int_class import IntegrateThread\nimport math", number=10), 4)))
